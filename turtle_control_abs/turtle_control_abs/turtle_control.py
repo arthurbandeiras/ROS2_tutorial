e029a84426d7683f14b963799403de71543e6ec5
arthurbandeiras <116594197+arthurbandeiras@users.noreply.github.com>
2024-10-09 12:46:34 -0300

    TODO: pub_callback()

--- turtle_control_abs/turtle_control_abs/turtle_control.py
+++ turtle_control_abs/turtle_control_abs/turtle_control.py
@@ -12,38 +12,56 @@ class TurtleControlNode(Node):
         self.init_subscribers()
         self.init_publisher()
 
-
-
-        timer_period = 1.0  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        timer_period = 0.1  # seconds
+        self.timer = self.create_timer(timer_period, self.pub_callback)
     
+
     def init_variables(self):
         self.x = 0
         self.y = 0
+        self.theta = 0
+        self.x_goal = 0
+        self.y_goal = 0
+        self.theta_goal = 0
 
 
-    def timer_callback(self):
-        pass
-    
     def init_publisher(self):
         self.publisher = self.create_publisher(Twist, "/abs/turtle1/cmd_vel", 10)
+
     
     def init_subscribers(self):
+
+        self.create_subscription(
+            Pose, 
+            "/abs/turtle1/pose", 
+            self.pose_callback, 
+            10
+        )
         
         self.create_subscription(
-                Pose, 
-                "turtlesim/msg/Pose", 
-                self.pose_callback, 
-                10)
-        
+            Pose2D,
+            "/abs/goal",
+            self.goal_callback,
+            10
+        )
     
-    def pose_callback(self):
-        pass
     
-    def goal_callback(self):
-        pass
+    def pose_callback(self, msg):
+        self.x = msg.x
+        self.y = msg.y
+        self.theta = msg.theta
+
+        self.get_logger().info(f'pose info: x={self.x} y={self.y}, theta={self.theta}')
     
+
+    def goal_callback(self, msg):
+        self.x_goal = msg.x
+        self.y_goal = msg.y
+        self.theta_goal = msg.theta
+
+        self.get_logger().info(f'goal info: x={self.x_goal} y={self.y_goal}, theta={self.theta_goal}')
+    
+
     def pub_callback(self):
         pass
 
